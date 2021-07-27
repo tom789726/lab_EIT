69a722eba2b16194051bfa617f60d7cf0c5c2e8d
match <tom789726@gmail.com>
2021-07-27 18:05:30 +0800

    First commit 0727 BINIL

---
+++ EIT_mesh.m
@@ -0,0 +1,86 @@
+clc; clear all; close all;
+
+%% Import Domain (?)
+sz = 256;
+
+img = imread('circle.png');
+img_bw = double(img(:,:,1)); % Take 1 dimension
+img_bw = imresize(img_bw,[sz sz]); % resize 
+img1 = (img_bw > max(img_bw(:))/2); % binarize
+img2 = edge(img1,'canny'); % edge detection (boundary extraction)
+se = offsetstrel('ball',5,5);
+img3 = imdilate(double(img2),se); % dilation (opt.)
+
+% figure
+% subplot(1,2,1);
+% imshow(img1,[]); 
+% subplot(1,2,2);
+% imshow(img2,[]);
+
+%% mesh generation
+L = 32; % no. of electrodes
+N = L*(L-1)/2; % no. of elements
+
+
+mid = sz/2; % center point
+node = zeros(2,N); % coordinate of nodes
+mesh = zeros(3,N); % node of meshes
+
+im_mesh = zeros(sz);
+im_mesh(mid,mid) = 1;
+r_max = round(225/2); % radius of outer circle
+flag = 0; % for alternating angles in each ring
+count = 0; % node record
+for r = 0:(r_max/5):r_max % create 5 rings of nodes
+    for i = 0:L % create L nodes each ring
+        if (r == 0)
+            break;
+        end
+        ang = (2*pi/L)*i;
+        if (flag)
+            ang = (2*pi/L)*i - (2*pi/L)/2;
+        end        
+        next_x = round( r*cos(ang) );
+        next_y = round( r*sin(ang) );
+        row = mid + next_y;
+        col = mid + next_x;
+        
+        count = count+1;
+        node(1,count) = row;
+        node(2,count) = col;
+        
+        im_mesh(row,col) = 1;
+    end
+    flag = rem(flag+1,2);
+end
+
+figure
+% subplot(1,2,1);
+% imshow(im_mesh,[]);
+% subplot(1,2,2);
+for r = 0:4
+    for i = 1:L
+        p = r*L+i;
+        plot(node(1,p:p+1),node(2,p:p+1),'-k'); hold on % connect same ring
+        % connect prev ring
+        if (r == 0)
+            plot([node(1,p:p+1),mid],[node(2,p:p+1),mid],'-k'); hold on
+        else
+            plot([node(1,p:p+1), node(1,(r-1)*L+i)],[node(2,p:p+1), node(2,(r-1)*L+i)],'-k'); hold on
+        end
+        
+        pause(0.1);
+    end
+end
+
+% 3.記得入番落mesh matrix記低
+
+%% phantom test
+
+phantom = imread('phantom.png');
+
+% figure
+% imshow(phantom);
+
+
+
---
+++ tut_breathingBaby.m
@@ -0,0 +1,46 @@
+clear all; close all; clc;
+run C:\Users\tom78\Documents\MATLAB\eidors-v3.10-ng\eidors\startup.m
+
+
+% Download Data
+% 直接上serverdownload, if你自己已經downlod, 就直接改unzip個path啦
+% zn = 'if-neonate-spontaneous.zip';
+% urlwrite(['http://eidors3d.sf.net/data_contrib/if-neonate-spontaneous/',zn],zn);
+% zipfilecontents = unzip(zn);
+
+% Select Reconstruction Algorithm
+fmdl = mdl_normalize(mk_library_model('neonate_16el'),1);
+[fmdl.stimulation,fmdl.meas_select] = mk_stim_patterns(16,1,'{ad}','{ad}');
+opt.imgsz = [64 64]; opt.noise_figure = 0.5;
+imdl = mk_GREIT_model(mk_image(fmdl,1), 0.25, [], opt);
+
+% Show reconstructed image
+vv= eidors_readdata('P04P-1016.get'); vi=vv(:,45); vh=vv(:,61);
+imr = inv_solve(imdl,vh,vi);
+
+clf; axes('position',[0.05,0.5,0.25,0.45]);
+imr.calc_colours = struct('ref_level',0,'greylev',0.2,'backgnd',[1,1,1]);
+show_slices(imr);
+
+% Indicate points to show waveforms
+yposns = [45  20 50]; xposns = [50  40 27]; ofs= [0,22,15];
+
+% Show positions on image
+hold on; for i = 1:length(xposns)
+    plot(xposns(i),yposns(i),'s','LineWidth',10);
+end; hold off;
+
+%% Show waveforms in the lungs and heart
+% Show plots
+imgs = calc_slices(inv_solve(imdl, vh, vv));
+axes('position',[0.32,0.6,0.63,0.25]);
+
+taxis =  (0:size(imgs,3)-1)/13; % frame rate = 13
+hold all
+for i = 1:length(xposns);
+    plot(taxis,ofs(i)+squeeze(imgs(yposns(i),xposns(i),:)),'LineWidth',2);
+end
+hold off
+set(gca,'yticklabel',[]); xlim([0 16]);
+
+print_convert neonate_intro04a.jpg
