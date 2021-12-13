0f92dc0be64641e8d3e8d15f48ec49f058464605
match <tom789726@gmail.com>
2021-12-14 03:37:19 +0800

    modify tutorials HOME

--- EIT_HumanArm.m
+++ EIT_HumanArm.m
@@ -6,11 +6,39 @@ pathname = 'Rods_arm_1981.xls';
 raw = importdata(pathname);
 rawData = getfield(raw,'data','Sheet1');
 
-vh = rawData(:,3); vh_rep = repmat(vh,[2,1]);
-vi = rawData(:,6); vi_rep = repmat(vi,[2,1]);
+vh = rawData(:,3);
+vi = rawData(:,6);
 
-ph = phantom(512);
+% Try 1
+% vh_rep = repmat(vh,[2,1]);
+% vi_rep = repmat(vi,[2,1]);
 
+% Try 2
+vh_rep = zeros(size(vh,1)*2,1);
+vi_rep = zeros(size(vi,1)*2,1);
+
+N = 16-3;
+
+A=[0:16:208];
+count = 1;
+
+clc
+for idx = 1:N
+    if (idx == N)
+        count = 104;
+    end
+    idx_data = [count:count+N-idx];
+    idx_rep = [1:size(idx_data,2)]+A(idx);
+%     vh_rep(idx_rep) = vh(idx_data);
+%     vi_rep(idx_rep) = vi(idx_data);
+
+    count = count+N-idx+1;    
+end
+
+figure
+subplot(2,1,1); plot(vh); xlim([1;104]);
+subplot(2,1,2); plot(vh_rep); xlim([1;104]);
+ 
 %% EIDORS
 % run C:\Users\Tom\Documents\MATLAB\eidors-v3.10-ng\eidors\startup.m
 
@@ -41,6 +69,10 @@ vh2 = real(v_reference)/1e4;
 pkpk2 = max(vh2(:));
 [pks2,locs2] = findpeaks(abs(vh2-pkpk2));
 
+% Optimum current pattern
+v_opt = ones(size(vh2));
+v_opt = v_opt.*cos((1:256)'*2*pi/16);
+
 figure
 subplot(311); plot(vh_norm); 
 title('Measurements (Human Arm, 1981)'); xlabel('pt'); ylabel('Potential(V)'); hold on;
@@ -53,8 +85,19 @@ plot(locs2,vh2(locs2),'rx');
 xticks([1:3:256]); legend('Measurement','Transition point');
 xlim([1,128]);
 
-subplot(313); plot(A); title('fwd\_model.meas\_select'); xlabel('electrode'); 
-xticks([1:2:64]); xlim([1,64]);
+subplot(313); plot(v_opt); 
+xlim([1,128]);
 
+figure 
+plot(vh2); hold on
+plot(v_opt);
+xlim([1,128]);
 
+%%
+imdl = mk_common_model('c2c',16);
+imdl.fwd_model.stimulation = mk_stim_patterns(16,1,[1,0],[0,1],{'meas_current'},1);
+imdl.fwd_model = rmfield( imdl.fwd_model, 'meas_select');
 
+img = inv_solve(imdl,real(v_reference)/1e4,v_opt);
+figure
+show_fem(img);
--- eidors_modeling.m
+++ eidors_modeling.m
@@ -2,7 +2,7 @@
 close all; clc;
 
 %% Launch
-username = "tom78"
+username = "Tom"
 pathname = fullfile("C:\Users\",username,"\Documents\MATLAB\eidors-v3.10-ng\eidors\startup.m");
 
 run(pathname)
@@ -12,17 +12,21 @@ run(pathname)
 
 load iirc_data_2006
 
-calc_colours('greylev',-.1);
-
+% Make FEM model
 imdl = mk_common_model('c2c');
+% Set stimulation patterns. Use meas_current
+% Stimulation of [1,0] (not [0,1]) is needed for this device (IIRC)
 imdl.fwd_model.stimulation = mk_stim_patterns(16,1,[1,0],[0,1],{'meas_current'},1);
+% Remove meas_select field because all 16x16 patterns are used
 imdl.fwd_model = rmfield( imdl.fwd_model, 'meas_select');
 
-vi= real(v_rotate(:,9))/1e4; 
+% Use loaded data (reference + inhomogeneous data)
+vi= real(v_rotate(:,9))/1e4;
 vh= real(v_reference)/1e4;
 
 
 for idx= 1:3
+   % By default, hyperparameter.value = 0.03
    if     idx==1
       imdl.hyperparameter.value= .03;
    elseif idx==2
@@ -31,24 +35,8 @@ for idx= 1:3
       imdl.hyperparameter.value= .10;
    end
    img= inv_solve(imdl, vh, vi);
-   img.calc_colours.greylev = -.3;
 
-   subplot(2,3,idx);
-   show_slices(img);
-
-   subplot(2,3,idx+3);
-   z=calc_slices(img);
-   c=calc_colours(z,img);
-   h=mesh(z,c); 
-   view(-11,44);
-   set(h,'CDataMapping','Direct');
-   set(gca,{'XLim','YLim','ZLim','XTickLabel','YTickLabel'}, ...
-        {[1 64],[1 64],[-3.3,0.5],[],[]})
+   subplot(1,3,idx); 
+   show_slices(img); title(string(imdl.hyperparameter.value));
 end
 
-%% Playground
-imdl = mk_common_model('c2c',16);
-vi= vi(1:208);
-vh = vh(1:208);
-
-img0 = inv_solve(imdl,vh,vi);
